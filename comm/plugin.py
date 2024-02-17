87c65855ab274cbdfbb0714ffe604f5684611330
Mauceri <cmauceri@gmail.com>
2024-02-17 20:52:39 +0100

    reload et get

--- comm/plugin.py
+++ comm/plugin.py
@@ -6,11 +6,9 @@ import os
 
 logger = logging.getLogger(__name__)
 
-class Load(IObserver):
+class Get(IObserver):
     def __init__(self,observable:IObservable=None):
         self.__observable =observable
-
-    import os
 
     def get_file_name(chemin):
         return os.path.basename(chemin)
@@ -22,17 +20,35 @@ class Load(IObserver):
         await self.__observable.notify(room,event,f"Fichier /data/plugins.yaml",path, filename)
 
     def prefix(self):
-        return "!c.load"
+        return "!c.get"
+    
+class Reload(IObserver):
+    def __init__(self,observable:IObservable=None):
+        self.__observable =observable
+
+    def get_file_name(chemin):
+        return os.path.basename(chemin)
+
+    async def notify(self,room:MatrixRoom, event:RoomMessageText, path:str):
+        logger.info(f"***************************** L'utilisateur {event.sender} a demandé un rechargement")
+        await self.__observable.notify(room,event,self.prefix(),None, None)
+
+    def prefix(self):
+        return "!c.reload"
     
 class Plugin(IPlugin):
     def __init__(self,observable:IObservable):
         self.__observable = observable
-        self.load = Load(self.__observable)
-        logger.info(f"********************** Observateur créé {self.load.prefix()}")
+        self.get = Get(self.__observable)
+        logger.info(f"********************** Observateur créé {self.get.prefix()}")
+        self.reload = Reload(self.__observable)
+        logger.info(f"********************** Observateur créé {self.reload.prefix()}")
         
     def start(self):
-        logger.info(f"********************** Inscripton de {self.load.prefix()}")
-        self.__observable.subscribe(self.load)
+        logger.info(f"********************** Inscripton de {self.get.prefix()}")
+        self.__observable.subscribe(self.get)
+        logger.info(f"********************** Inscripton de {self.reload.prefix()}")
+        self.__observable.subscribe(self.reload)
 
     async def stop(self):
         self.__observable.unsubscribe(self.load)
